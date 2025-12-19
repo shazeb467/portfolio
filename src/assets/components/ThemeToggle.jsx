a1d9a7471a849e8b2459464f049bf2e5fad129d6
mhd shazeb <mshazeb@phoenix.tech>
2025-12-19 13:39:01 +0530

    6th-commit: Theme Toggle added

---
+++ src/assets/components/ThemeToggle.jsx
@@ -0,0 +1,48 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+import { cn } from "../../lib/utils";
+
+const ThemeToggle = () => {
+  const storedTheme = localStorage.getItem("theme") === "dark";
+  const [isDarkMode, setIsDarkMode] = useState(storedTheme);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-30 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden",
+        "cursor-pointer"
+      )}
+      title={isDarkMode ? "Light" : "Dark"}
+    >
+      {isDarkMode ? (
+        <Sun className="w-6 h-6 text-yellow-300" />
+      ) : (
+        <Moon className="w-6 h-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggle;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import ThemeToggle from "../assets/components/ThemeToggle";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <h1 className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+    </h1>
+  );
 };
 
 export default Home;
