69ff88fdd5bbf38d0e969fd70f19ccb86b5a10d2
mhd shazeb <mshazeb@phoenix.tech>
2025-12-22 15:16:07 +0530

    renamed back href home as hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       <div className="container flex items-center justify-between">
         <a
           className="text-xl font-bold text-primary flex items-center"
-          href="#home"
+          href="#hero"
         >
           <span className="relative z-10">
             <span className="text-glow text-foreground">Shazeb </span>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const navItems = [
-  { name: "Home", href: "#home" },
+  { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
